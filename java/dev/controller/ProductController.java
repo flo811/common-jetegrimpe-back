4044ac354e6cd870d9c2d90a677b134910569961
florian <flo811@msn.com>
2018-12-11 18:09:08 +0100

    c

--- java/dev/controller/ProductController.java
+++ java/dev/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.controller.vm.ProductVM;
 import dev.repository.ProductRepo;
 
+@CrossOrigin
 @RestController()
 @RequestMapping("/product")
 public class ProductController {
