17c72555c6f1512af2864e588bc1ab5dcc713612
chloe <chloekaczmarek0@gmail.com>
2018-12-17 09:47:58 +0100

    commit back matin

--- src/main/java/dev/controller/ProductController.java
+++ src/main/java/dev/controller/ProductController.java
@@ -4,12 +4,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.controller.vm.ProductVM;
+import dev.domain.Product;
+import dev.domain.Role;
 import dev.repository.ProductRepo;
 import dev.service.ProductService;
 
@@ -35,4 +41,20 @@ public class ProductController {
 				.map(ProductVM::new)
 				.collect(Collectors.toList());
 	}
+	
+	
+	
+	// Modify a product
+	@Secured("ROLE_ADMINISTRATEUR")
+	@PatchMapping("/{name}")
+	public ProductVM patch(@PathVariable String name, @RequestBody Product productNew){
+		
+		Product productOld = productRepo.findByName(name);
+		System.out.println("djehdiede");
+		productNew.setId(productOld.getId());
+
+		productRepo.save(productNew); 
+		return new ProductVM(productNew);
+	}
+	
 }
